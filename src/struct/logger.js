6efd1a9f0c7990096f97e90c807d025233abccf7
nehalsci <nehal71.sci@gmail.com>
2024-01-07 21:42:33 +0600

    feat(Logger): init

---
+++ src/struct/logger.js
@@ -0,0 +1,13 @@
+"use strict";
+
+class Logger {
+	constructor(options) {
+		this.options = options;
+	}
+
+	act(type = 1, message = "", hook = false) {
+		return 0;
+	}
+}
+
+module.exports = Logger;
